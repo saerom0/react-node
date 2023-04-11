c5e67589ca4ce9d7c4af4c8e009e27c45172d815
saerom0 <jinxinsr@naver.com>
2023-04-11 12:58:29 +0900

    프론트에서 서버에 요청하여 응답객체를 다시 프론트에 전달

--- node/index.js
+++ node/index.js
@@ -4,24 +4,32 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
-//express에서 react안쪽 build폴더까지의 경로를 static으로 지정
 app.use(express.static(path.join(__dirname, '../react/build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//MongoDB 접속
 app.listen(port, () => {
 	mongoose
 		.connect(
-			'mongodb+srv://saerom:!abc1234 @cluster0.nfm6xba.mongodb.net/?retryWrites=true&w=majority'
+			'mongodb+srv://saerom:!abc1234@cluster0.nfm6xba.mongodb.net/?retryWrites=true&w=majority'
 		)
 		.then(() =>
-			console.log(`Server app listening on port ${port} with MongoDB`)
+			console.log(`Server app listening on Port ${port} with MongoDB`)
 		)
 		.catch((err) => console.log(err));
 });
 
+//기본 라우터 설정
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../react/build/index.html'));
 });
-
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../react/build/index.html'));
+});
+
+//react로 부터 받은 요청처리
+app.post('/api/send', (req, res) => {
+	console.log(req.body);
+	res.json({ success: true, result: req.body.name + '2' });
 });
--- react/src/App.js
+++ react/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const item = { name: 'David' };
+
+	useEffect(() => {
+		axios
+			.post('/api/send', item)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [item]);
+
+	return (
+		<div className='App'>
+			<h1>React</h1>
+		</div>
+	);
 }
 
 export default App;
