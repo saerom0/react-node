12309f87e9da68bf44817cb1db60e899d0ebe74d
saerom0 <jinxinsr@naver.com>
2023-04-14 12:53:00 +0900

    firebase정보값을 전역으로 저장할 userSlice 생성 후 App에 전역으로 전달

--- react/src/index.js
+++ react/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './redux/userSlice';
+
+const store = configureStore({
+	reducer: {
+		user: userSlice,
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
---
+++ react/src/redux/userSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState: {
+		displayName: '',
+		uid: '',
+		accessToken: '',
+	},
+	reducers: {
+		loginUser: (state, action) => {
+			state.displayName = action.payload.displayName;
+			state.uid = action.pay.uid;
+			state.accessToken = action.payload.accessToken;
+		},
+		logoutUser: (state) => {
+			state.displayName = '';
+			state.uid = '';
+			state.accessToken = '';
+		},
+	},
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+export default userSlice.reducer;
